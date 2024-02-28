b38bb91742bc3246afdf074114d05613edd66ef8
ArasiStefanoISS <stefano.arasi@studio.unibo.it>
2024-02-28 15:25:36 +0100

    ProdCons
    Primo commit analisi requisiti ProdCons

---
+++ issLab24/project0/src/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package main;
+
+import unibo.basicomm23.utils.CommUtils;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("HelloWorld from project0!");
+		CommUtils.outblue("HelloWorld again from project0!");
+	}
+	
+}
